732ce9ccbccf42317072d9502501ee7008579a23
Andrew Church <andrewchurch@Andrews-MacBook-Pro.local>
2017-08-01 09:07:02 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+    $(document).click(function() {
+    var $this = $(this);
+    if($this.attr('editing') != '1') {
+        
+        $(document).find('.editable').each(function() {
+            var input = $('<input class="editing" />').val($(this).text());
+            $(this).replaceWith(input)
+        });
+    }
+    else {
+        $this.text('Edit').removeAttr('editing')
+        $(document).find('input.editing').each(function() {
+            var div = $('<div class="editable" />').text($(this).val())
+            $(this).replaceWith(div)
+        })
+    }
+})
