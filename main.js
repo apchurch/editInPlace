d300030dc066de1969a100eebbb31a66a93a59c9
Andrew Church <andrewchurch@Andrews-MacBook-Pro.local>
2017-08-02 07:37:44 -0600

    Add existing file

--- main.js
+++ main.js
@@ -1,21 +1,65 @@
+$(document).ready(function(){
+    $('#myNameP').on('click', function(){
+        $('#myNameP').addClass('hidden')
+        $('#myNameInput').removeClass('hidden')
+        $('#myNameInput').trigger('focus')
+        $('#myNameInput').val($('#myNameP').text())
+    })
+
+    $('#myNameInput').on('blur', function(){
+        $('#myNameP').removeClass('hidden')
+        $('#myNameInput').addClass('hidden')
+        $('#myNameP').text($('#myNameInput').val())
+    })
+
+    $('#myBioP').on('click', function(){
+        $('#myBioP').addClass('hidden')
+        $('#myBioInput').removeClass('hidden')
+        $('#myBioInput').trigger('focus')
+        $('#myBioInput').val($('#myBioP').text())
+    })
+
+    $('#myBioInput').on('blur', function(){
+        $('#myBioP').removeClass('hidden')
+        $('#myBioInput').addClass('hidden')
+        $('#myBioP').text($('#myBioInput').val())
+    })
+      $('#myDateP').on('click', function(){
+        $('#myDateP').addClass('hidden')
+        $('#myDateInput').removeClass('hidden')
+        $('#myDateInput').trigger('focus')
+        $('#myDateInput').val($('#myDateP').text())
+    })
+
+    $('#myDateInput').on('blur', function(){
+        $('#myDateP').removeClass('hidden')
+        $('#myDateInput').addClass('hidden')
+        $('#myDateP').text($('#myDateInput').val())
+    })
+    $('#myJobP').on('click', function(){
+        $('#myJobP').addClass('hidden')
+        $('#myJobInput').removeClass('hidden')
+        $('#myJobInput').trigger('focus')
+        $('#myJobInput').val($('#myJobP').text())
+    })
+
+    $('#myJobInput').on('blur', function(){
+        $('#myJobP').removeClass('hidden')
+        $('#myJobInput').addClass('hidden')
+        $('#myJobP').text($('#myJobInput').val())
+    })
+    $('#myCollegeP').on('click', function(){
+        $('#myCollegeP').addClass('hidden')
+        $('#myCollegeInput').removeClass('hidden')
+        $('#myCollegeInput').trigger('focus')
+        $('#myCollegeInput').val($('#myCollegeP').text())
+    })
+
+    $('#myCollegeInput').on('blur', function(){
+        $('#myCollegeP').removeClass('hidden')
+        $('#myCollegeInput').addClass('hidden')
+        $('#myCollegeP').text($('#myCollegeInput').val())
+    })
+})
 
 
-$(document).ready(function() {
-    $('.editable').on('click', function() {
-    var $this = $(this);
-    if($this.attr('editing') != '1') {
-        
-        $(document).find('.editable').each(function() {
-            var input = $('<input class="editing" />').val($(this).text());
-            $(this).replaceWith(input)
-        });
-    }
-    else {
-        $this.text('Edit').removeAttr('editing')
-        $(document).find('input.editing').each(function() {
-            var div = $('<div class="editable" />').text($(this).val())
-            $(this).replaceWith(div)
-        })
-    }
-})
-})
